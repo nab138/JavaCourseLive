5e2c802b9957b2bba87bb7a9e43f9406d573c582
nab138 <nab@nabdev.me>
2025-07-02 18:43:32 -0400

    Update config.tsx

--- client/src/config.tsx
+++ client/src/config.tsx
@@ -1,3 +1,3 @@
-const PROD_WS_URL = "wss://javacourseliveapi.serverpit.com:3001/";
+const PROD_WS_URL = "wss://javacourselive.onrender.com";
 const DEV_WS_URL = "ws://localhost:3001/";
 export const WS_URL = import.meta.env.DEV ? DEV_WS_URL : PROD_WS_URL;
